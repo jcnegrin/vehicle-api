7019a559843bb0321fe5e4bf2cd29cf56d2d6286
Juan Negrin <jcnegrin@icloud.com>
2021-03-14 15:03:55 +0100

    First commit

---
+++ pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -0,0 +1,25 @@
+package com.udacity.pricing.api;
+
+import com.udacity.pricing.domain.price.Price;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class PricingControllerTest {
+
+    @Test
+    public void getCarPrice() {
+
+    }
+}
---
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -0,0 +1,94 @@
+package com.udacity.vehicles.service;
+
+import com.udacity.vehicles.client.maps.MapsClient;
+import com.udacity.vehicles.client.prices.PriceClient;
+import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.domain.car.CarRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implements the car service create, read, update or delete
+ * information about vehicles, as well as gather related
+ * location and price data when desired.
+ */
+@Service
+public class CarService {
+
+    private final CarRepository repository;
+
+    @Autowired
+    private PriceClient priceClient;
+
+    @Autowired
+    private MapsClient mapsClient;
+
+    public CarService(CarRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Gathers a list of all vehicles
+     * @return a list of all vehicles in the CarRepository
+     */
+    public List<Car> list() {
+        return repository.findAll();
+    }
+
+    /**
+     * Gets car information by ID (or throws exception if non-existent)
+     * @param id the ID number of the car to gather information on
+     * @return the requested car's information, including location and price
+     */
+    public Car findById(Long id) {
+
+        Optional<Car> optionalCar = repository.findById(id);
+
+        if (optionalCar.isEmpty()) {
+            throw new CarNotFoundException("There is not a car with ID: " + id);
+        }
+
+        final Car car = optionalCar.get();
+        car.setPrice(priceClient.getPrice(car.getId()));
+        car.setLocation(mapsClient.getAddress(car.getLocation()));
+
+        return car;
+    }
+
+    /**
+     * Either creates or updates a vehicle, based on prior existence of car
+     * @param car A car object, which can be either new or existing
+     * @return the new/updated car is stored in the repository
+     */
+    public Car save(Car car) {
+        if (car.getId() != null) {
+            return repository.findById(car.getId())
+                    .map(carToBeUpdated -> {
+                        carToBeUpdated.setDetails(car.getDetails());
+                        carToBeUpdated.setLocation(car.getLocation());
+                        return repository.save(carToBeUpdated);
+                    }).orElseThrow(CarNotFoundException::new);
+        }
+
+        return repository.save(car);
+    }
+
+    /**
+     * Deletes a given car by ID
+     * @param id the ID number of the car to delete
+     */
+    public void delete(Long id) {
+
+        Optional<Car> optionalCar = repository.findById(id);
+
+        if (optionalCar.isEmpty()) {
+            throw new CarNotFoundException("There is not a car with ID: " + id);
+        }
+
+        repository.delete(optionalCar.get());
+
+    }
+}
