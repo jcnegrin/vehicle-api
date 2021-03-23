47a26e89a49b4843920ea6b0f424ade959263f0c
Juan Negrin <jcnegrin@icloud.com>
2021-03-23 12:48:47 +0100

    Added pricing controller test

--- pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
+++ pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -1,25 +1,39 @@
 package com.udacity.pricing.api;
 
-import com.udacity.pricing.domain.price.Price;
-import com.udacity.pricing.service.PricingService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = PricingController.class)
+@AutoConfigureMockMvc
 public class PricingControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public void getCarPrice() {
+    public void getCarPrice() throws Exception {
+        Long vehicleId = 1L;
+        mockMvc.perform(get("/services/price?vehicleId=" + vehicleId)
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.currency").value("USD"))
+                .andExpect(jsonPath("$.price").exists())
+                .andExpect(jsonPath("$.vehicleId").value(1));
 
     }
 }
